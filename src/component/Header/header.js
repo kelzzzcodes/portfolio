a4107f6875a1dba3e81a9e4f5cf63b909e629c07
Kelechi Otugbuali <hilakechi@gmail.com>
2023-02-24 02:47:45 +0100

    header.js refactored

--- src/component/Header/header.js
+++ src/component/Header/header.js
@@ -1,32 +1,71 @@
 import { React, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { navdata } from '../../constants'
-// import MenuIcon from '@mui/icons-material/Menu'
-// import CloseIcon from '@mui/icons-material/Close'
+import MenuIcon from '@mui/icons-material/Menu'
+import CloseIcon from '@mui/icons-material/Close'
+
+const Menu =()=>(
+  <div className=" w-full flex justify-center align-center p-8 bounce-in-fwd sm:hidden">
+  <ul className="flex flex-col space-y-8 text-white align-center justify-center ">
+    {navdata.map((nav, index) => (
+      <li
+        key={nav.id}
+        className=" flex items-center cursor-pointer border-b-2 border-[#4CD794] hover:border-b-2 hover:border-black hover:text-gray-500"
+      >
+        <Link to={nav.path}>
+          {nav.title} {nav.Icon}
+        </Link>{' '}
+      </li>
+    ))}
+  </ul>
+</div>
+
+)
 
 function header() {
+  const [toggleMenu, setToggleMenu] = useState(false)
   return (
-    <nav className="p-2 flex rounded-3xl justify-between items-center content-center w-full h-16  bg-[#4CD794] text-black sm:flex-col sm:space-y-4  sm:h-32 sm:p-10 sm:items-baseline sm:rounded-b-3xl  sm:rounded-t-none ">
-      <div className="flex justify-center align-center items-center  sm:justify-start  ">
-        <Link to='/' className="flex  items-center space-x-2 ">
-          <span className="text-lg">Kelechi Otugbuali</span>
-        </Link>
+   <nav className=' bg-[#4CD794]  w-full h-16 flex flex-col p-4 space-y-4 rounded-3xl sm:rounded-t-lg md:h-32 md:p-8  '>
+    <div className='flex justify-between'>
+    <div className='text-black'>
+    <Link to=''>KELECHI OTUGBUALI</Link>
+    </div>
+      <div className='flex  md:hidden'>
+      {toggleMenu ? (
+            <CloseIcon
+              onClick={() => setToggleMenu(false)}
+              className="cursor-pointer"
+            />
+          ) : (
+            <MenuIcon
+              onClick={() => setToggleMenu(true)}
+              className="cursor-pointer"
+            />
+          )}
+
+          
+
       </div>
-      <div className="hidden sm:flex">
-        <ul className="flex space-x-4 text-white">
-          {navdata.map((nav, index) => (
-            <li
-              key={nav.id}
-              className=" flex items-center justify-center space-x-2 cursor-pointer border-b-2 border-[#4CD794] hover:border-b-2 hover:border-black hover:text-gray-500"
-            >
-              <Link to={nav.path}>
-                {nav.title} {nav.Icon}
-              </Link>{' '}
-            </li>
-          ))}
-        </ul>
-      </div>
-    </nav>
+    
+    </div>
+
+    <div className='hidden md:flex'>
+    <ul className="flex align-center justify-start space-x-4 text-white">
+    {navdata.map((nav, index) => (
+      <li
+        key={nav.id}
+        className=" flex items-center cursor-pointer border-b-2 border-[#4CD794] hover:border-b-2 hover:border-black hover:text-gray-500"
+      >
+        <Link to={nav.path}>
+          {nav.title} {nav.Icon}
+        </Link>{' '}
+      </li>
+    ))}
+  </ul>
+
+    </div>
+    {toggleMenu ? <Menu /> : null}
+   </nav>
   )
 }
 
